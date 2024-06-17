e1bb0915f9887bcf888fe9b76f2e0d838b3f36e8
Harshada <vikheharshada@gmail.com>
2024-06-17 16:22:32 +0530

    Made changes to improve user interface

--- src/components/CustomWebcam.js
+++ src/components/CustomWebcam.js
@@ -1,60 +1,128 @@
-import React, { useCallback, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 import { useState } from "react";
+import frameSrc from "../assets/frame.png";
 
 const CustomWebcam = () => {
   const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
+  const [framedImage, setFramedImage] = useState(null);
+  const [canvasSize, setCanvasSize] = useState({ width: 500, height: 530 });
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot({
-      width: 300,
-      height: 330,
+      width: canvasSize.width,
+      height: canvasSize.height,
       quality: 1.0,
     });
     setImgSrc(imageSrc);
-  }, [webcamRef]);
+  }, [webcamRef, canvasSize]);
+
+  const addFrame = useCallback(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const photo = new Image();
+    const frame = new Image();
+
+    photo.onload = () => {
+      canvas.width = canvasSize.width;
+      canvas.height = canvasSize.height;
+      ctx.drawImage(photo, 0, 0, canvasSize.width, canvasSize.height);
+
+      frame.onload = () => {
+        ctx.drawImage(frame, -6, 0, 510, 540);
+        const framedImageSrc = canvas.toDataURL("image/jpeg");
+        setFramedImage(framedImageSrc);
+      };
+      frame.src = frameSrc;
+    };
+    photo.src = imgSrc;
+  }, [imgSrc, frameSrc, canvasSize]);
+
+  useEffect(() => {
+    if (imgSrc) {
+      addFrame();
+    }
+  }, [imgSrc, addFrame]);
 
   const retake = () => {
     setImgSrc(null);
+    setFramedImage(null);
   };
+
+  const downloadFramedImage = () => {
+    if (framedImage) {
+      const link = document.createElement("a");
+      link.href = framedImage;
+      link.download = "framed-photo.jpg";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const frame = new Image();
+      frame.onload = () => {
+        setCanvasSize({ width: frame.width, height: frame.height });
+      };
+      frame.src = frameSrc;
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [frameSrc]);
 
   return (
     <div className="container">
-      <img src={imgSrc} alt="img" className="webcamImg" />
+      {imgSrc && <img src={imgSrc} alt="img" className="webcamImg" />}
       <Webcam
         ref={webcamRef}
         className="webcam-view"
         screenshotQuality={1}
         videoConstraints={{
-          width: 300,
-          height: 330,
-          aspectRatio: 0.5,
+          width: canvasSize.width,
+          height: canvasSize.height,
+          aspectRatio: canvasSize.width / canvasSize.height,
           facingMode: "user",
         }}
       />
+
       <div className="buttons">
         <div className="custom-button">
-          <button onClick={capture}>
+          <button className="btn1" onClick={capture}>
             CAPTURE
             <div className="line"></div>
           </button>
         </div>
         <div className="custom-button">
-          <button onClick={retake}>
+          <button className="btn2" onClick={retake}>
             RETAKE
             <div className="line"></div>
           </button>
         </div>
         <div className="custom-button">
-          <a href={imgSrc}  download="webcam-photo.jpg">
-            <button>
-              SAVE
-              <div className="line"></div>
-            </button>
-          </a>
+          <button className="btn3">
+            POST
+            <div className="line"></div>
+          </button>
+        </div>
+
+        <div className="custom-button">
+          <button className="btn4" onClick={downloadFramedImage}>
+            SAVE
+            <div className="line"></div>
+          </button>
         </div>
       </div>
+      <canvas
+        ref={canvasRef}
+        style={{ display: "none" }}
+        width={canvasSize.width}
+        height={canvasSize.height}
+      ></canvas>
     </div>
   );
 };
--- src/components/Photobooth.js
+++ src/components/Photobooth.js
@@ -3,6 +3,7 @@ import bgImg from "../assets/bg.jpg";
 import rightImg from "../assets/right-img.png";
 import frameImg from "../assets/frame.png";
 import Home from "./Home";
+import CustomWebcam from "./CustomWebcam";
 
 const Photobooth = () => {
   return (
@@ -10,7 +11,9 @@ const Photobooth = () => {
       <img className="bgImg" src={bgImg} alt="bg" />
       <img className="rightImg" src={rightImg} alt="girlImage" />
       <Home />
+      
       <img className="frameImg" src={frameImg} alt="Frame" />
+      <CustomWebcam frameSrc={frameImg} />
     </div>
   );
 };
